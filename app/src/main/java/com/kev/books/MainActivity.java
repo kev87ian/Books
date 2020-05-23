e525e01d8061b431f0d1d6067fc6a147ecb6633e
kev87ian <kelvinian87@gmail.com>
2020-05-23 10:28:05 +0300

    redid colors on index page for better visibility

--- app/src/main/java/com/kev/books/MainActivity.java
+++ app/src/main/java/com/kev/books/MainActivity.java
@@ -3,12 +3,16 @@ package com.kev.books;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindBooksButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       mFindBooksButton = (Button)findViewById(R.id.findBooksButton);
     }
 }
