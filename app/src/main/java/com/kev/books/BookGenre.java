61767b67dc48feea0d0d8fae6d12f0aa5a42322f
kev87ian <kelvinian87@gmail.com>
2020-05-24 19:06:44 +0300

    added array adapter

--- app/src/main/java/com/kev/books/BookGenre.java
+++ app/src/main/java/com/kev/books/BookGenre.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
 
 public class BookGenre extends AppCompatActivity {
     private TextView mTitleTextView;
+    private ListView mListView;
+    private String[] Books = new String[] {"The Fault in Our Stars", "The Subtle Art of Not Giving"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +20,13 @@ public class BookGenre extends AppCompatActivity {
         setContentView(R.layout.activity_book_genre);
 
         mTitleTextView = (TextView) findViewById(R.id.titleTextView);
+        mListView = (ListView)findViewById(R.id.listView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Books);
+        mListView.setAdapter(adapter);
+
         Intent intent = getIntent();
         String title = intent.getStringExtra("title");
-        mTitleTextView.setText("These are all the books with " + title + " " + "in their titles");
-
+        mTitleTextView.setText("Here are all the titles available related to " + title);
     }
 }
